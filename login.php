e9d8e52d25dd0cfe4606b96f353c5724d04d573d
hurt13 <hurttttt13@gmail.com>
2024-06-18 13:42:04 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+    echo "Username: " . $username . "<br>";
+    echo "Password: " . $password . "<br>";
+} else {
+    echo "Invalid request.";
+}
+?>
